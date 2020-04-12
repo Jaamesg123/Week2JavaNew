c9a512d8e0e92da3582146c352f7b03303b5f893
Jammersg <james.alexander.gallaher@gmail.com>
2020-04-11 19:52:01 -0700

    edited loops file

--- src/LoopsWeek2.java
+++ src/LoopsWeek2.java
@@ -2,20 +2,37 @@
 public class LoopsWeek2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		for (int a = 1; a <= 100; a++) {
-			if (a % 15 == 0) {
+	
+		int a = 0;
+		int b = 100;
+		
+		while (a <= 100) {
+			System.out.println(a);
+			a += 2;
+		}
+		
+		while (b >= 0) {
+			System.out.println(b);
+			b -= 3;
+		}
+		
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+			i += 1;	
+		}
+		
+		for (int i = 1; i <= 100; i++) {
+			if (i % 15 == 0) {
 				System.out.println("Hello World");
 			}
-			else if (a % 3 == 0) {
+			else if (i % 3 == 0) {
 				System.out.println("Hello");
 			}
-			else if (a % 5 == 0) {
+			else if (i % 5 == 0) {
 				System.out.println("World");
 				
 			} else {
-			System.out.println(a);
+			System.out.println(i);
 		}
 	
 	}
